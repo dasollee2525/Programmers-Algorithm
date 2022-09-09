c31debcdbb26535259ce085d3f3687de5abee78d
dasollee2525 <90080808+dasollee2525@users.noreply.github.com>
2022-09-10 00:06:34 +0900

    Update [BFS]ChangingWord.py

--- DFS-BFS/[BFS]ChangingWord.py
+++ DFS-BFS/[BFS]ChangingWord.py
@@ -5,22 +5,24 @@ def solution(begin, target, words):
     #BFS(시작 지점에서 가까운 노드부터 차례대로 그래프의 모든 노드를 탐색)
     if target not in words:
         return 0
-    
+
     words_num = len(words)
     word_len = len(begin)
-    visited=[False]*(words_num)
+    visited = [False]*(words_num)
     queue = deque([(begin,0)])
-    
+
+    answer = float('inf')
     while queue:
         w, cnt = queue.popleft()
         if w == target:
             answer = min(answer, cnt)
-        for j in range(words_num):
+        for i in range(words_num):
             count = 0
-            for i in range(word_len):
-                if words[i][j] != w[i]:
+            for j in range(word_len):
+                if words[i][j] != w[j]:
                     count+=1
-            if count == 1 and not visited[j]:
-                  visited[j] = True
-                  queue.append((words[j],cnt+1))              
-    return answer        
+            if count == 1 and not visited[i]:
+                  visited[i] = True
+                  queue.append((words[i],cnt+1))              
+    return answer    
+#2차원 배열에서는 인덱스를 조심
