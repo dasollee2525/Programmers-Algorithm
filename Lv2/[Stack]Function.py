f532b62d3791d7e8aa139fe5b1e55b9df5ca858e
dasollee2525 <90080808+dasollee2525@users.noreply.github.com>
2022-09-20 01:22:32 +0900

    Update [Stack]Function.py

--- Lv2/[Stack]Function.py
+++ Lv2/[Stack]Function.py
@@ -1,16 +1,18 @@
 def solution(progresses, speeds):
-    tasks = [(100-p)//s if (100-p)%s > 0 else (100-p)//s+1 for p, s in zip(progresses, speeds)]
+    tasks = [(100-p)//s+1 if (100-p)%s > 0 else (100-p)//s for p, s in zip(progresses, speeds)]
     answer = []
     while tasks:
         count = 0
         tasks = [i-tasks[0] for i in tasks]
-        print(tasks)
         tasks.pop(0)
         count = count + 1
-        if tasks:
+        if len(tasks) == 0:
+            answer.append(count)
             break
         while tasks[0] <= 0:
             tasks.pop(0)
             count = count + 1
+            if len(tasks) == 0:
+                break
         answer.append(count)
     return answer
